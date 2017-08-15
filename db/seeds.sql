6a615da23c63bcf0e6257c43255cc713ed8bc634
harbasheng <jack.bashirian@harbash.com>
2017-08-15 00:20:18 -0400

    rev 0

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("BURGER", FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ("BURGER2", FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ("BURGER3", FALSE);
